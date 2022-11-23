dafda3d3c90f4a3b0a8f57b20652fcd793084f6b
Agustin <agrillo@itba.edu.ar>
2022-11-23 19:49:28 -0300

    isolate observable interface from spawnable interface

---
+++ particle_filter/src/world/interfaces/Spawnable.java
@@ -0,0 +1,12 @@
+package world.interfaces;
+
+
+/**
+ * Spawnable interface
+ */
+public interface Spawnable {
+
+    public float[] getXYPosition();
+    public String getID();
+    public String getSymbol();
+}
