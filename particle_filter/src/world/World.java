2add499504095e641d4b6da7c56b9504207891b8
Agustin <agrillo@itba.edu.ar>
2022-11-26 18:32:36 -0300

    implement observer (WIP)

--- particle_filter/src/world/World.java
+++ particle_filter/src/world/World.java
@@ -1,22 +1,30 @@
 package world;
 
+import world.interfaces.Observer;
+import world.interfaces.Observable;
 import world.spawnables.*;
+import java.util.ArrayList;
 import java.util.Random;
 
 
 /**
  * World
  */
-public class World {
-    // TODO: world
-    // - Generalize class to multiple (variable) assets/elements
+public class World implements Observer{
+    // TODO: Observer
+    // - Create array to store observables (items)
+    // - Implement update method
 
-    private int width = 60;
-    private int height = 20;
+    private float width = 6;        // Coordinate
+    private float height = 2;       // Coordinate
+    private int render_rows = 60;   // Cell
+    private int render_cols = 20;   // Cell
+    public BaseSpawnable[] radars;
+    public BaseSpawnable[] robots;
+    BaseSpawnable[][] empty_world = new BaseSpawnable[render_rows][render_cols];
+    BaseSpawnable[][] world;
 
     Random randomizer = new Random();
-    Robot robot;
-    Tower tower1, tower2;
 
     // Terminal coloring
     static final String ANSI_RESET = "\u001B[0m";
@@ -31,38 +39,60 @@ public class World {
     static final String ANSI_WHITE = "\u001B[37m";
 
     public World(){
-        robot = new Robot(randomizer.nextInt(width), randomizer.nextInt(height));
+        // robot = new Robot(randomizer.nextInt(width), randomizer.nextInt(height));
+        //
+        // int tow_x, tow_y;
+        // do {
+        //     tow_x = randomizer.nextInt(width);
+        //     tow_y = randomizer.nextInt(height);
+        // } while (tow_x == robot.x && tow_y == robot.y);
+        // tower1 = new Tower(tow_x, tow_y);
+        // do {
+        //     tow_x = randomizer.nextInt(width);
+        //     tow_y = randomizer.nextInt(height);
+        // } while ((tow_x == robot.x && tow_y == robot.y) || (tow_x == tower1.x && tow_y == tower1.y));
+        // tower2 = new Tower(tow_x, tow_y);
+    }
 
-        int tow_x, tow_y;
-        do {
-            tow_x = randomizer.nextInt(width);
-            tow_y = randomizer.nextInt(height);
-        } while (tow_x == robot.x && tow_y == robot.y);
-        tower1 = new Tower(tow_x, tow_y);
-        do {
-            tow_x = randomizer.nextInt(width);
-            tow_y = randomizer.nextInt(height);
-        } while ((tow_x == robot.x && tow_y == robot.y) || (tow_x == tower1.x && tow_y == tower1.y));
-        tower2 = new Tower(tow_x, tow_y);
+    public void update(Observable o){
+        // TODO:
+
+    }
+
+    private void createEmptyWorld(){
+        // TODO: Add floor elements
+        // Overkill??
+
+    }
+
+    private float[] cell2Coord(int[] cell){
+        // TODO:
+        return new float[]{0, 0};
+
+    }
+
+    private int[] coord2Cell(float[] coord){
+        // TODO:
+        return new int[]{0, 0};
 
     }
 
     public void plot(){
-        String c;
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
-                if (i == robot.y && j == robot.x) {
-                    c = ANSI_GREEN + "º" + ANSI_RESET;
-                } else if ((i == tower1.y && j == tower1.x) || (i == tower2.y && j == tower2.x)) {
-                    c = ANSI_YELLOW + "×" + ANSI_RESET;
-                } else {
-                    // c = "•";
-                    c = ANSI_GREY + "·" + ANSI_RESET;
-                }
-                System.out.print(c);
-            }
-            System.out.println("");
-        }
+        // String c;
+        // for (int i = 0; i < height; i++) {
+        //     for (int j = 0; j < width; j++) {
+        //         if (i == robot.y && j == robot.x) {
+        //             c = ANSI_GREEN + "º" + ANSI_RESET;
+        //         } else if ((i == tower1.y && j == tower1.x) || (i == tower2.y && j == tower2.x)) {
+        //             c = ANSI_YELLOW + "×" + ANSI_RESET;
+        //         } else {
+        //             // c = "•";
+        //             c = ANSI_GREY + "·" + ANSI_RESET;
+        //         }
+        //         System.out.print(c);
+        //     }
+        //     System.out.println("");
+        // }
     }
     
 }
